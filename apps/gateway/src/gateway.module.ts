f5152d95d06b944e79bbe78829671cda18227a50
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-06 20:46:56 +0700

    update gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
 import { CommonModule } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
 
@@ -12,7 +10,5 @@ import { ConfigModule } from '@nestjs/config';
             envFilePath: 'apps/gateway/.env',
         }),
     ],
-    controllers: [GatewayController],
-    providers: [GatewayService],
 })
 export class GatewayModule {}
