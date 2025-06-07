f75c7a072e345d3154726d0cfe2d430a252211d2
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-07 10:55:36 +0700

    init openapi in gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from 'winston';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
 import { ErrorFilter } from 'libs/common/filter/error.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(GatewayModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
         app.get(ConfigService).get<number | undefined>('PORT') || 3000;
 
     app.useGlobalFilters(new ErrorFilter());
+
+    const config = new DocumentBuilder()
+        .setTitle('Task App')
+        .setDescription('Simple application for making task and note')
+        .setVersion('1.0.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(port);
 }
